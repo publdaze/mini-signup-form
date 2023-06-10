de9f1c609384e1f0b90f5c20a04af1a7ed3036bc
publdaze <publdaze@naver.com>
2023-06-10 16:20:20 +0900

    feat: 페이지 로드 됐을 때 ID 필드에 focus 되도록 처리

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const idEl = document.getElementById('id')
 const pwEl = document.getElementById('pw')
 const pwCheckEl = document.getElementById('pw-check')
+
+const setFocus = (el) => {
+    el.focus()
+}
+
+setFocus(idEl)
