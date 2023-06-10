272880f29e7befe0ebd72cba181ca0c342445f8c
publdaze <publdaze@naver.com>
2023-06-11 02:23:04 +0900

    feat: id 유효성 검사
    - 빈 값일 경우 : '필수 정보입니다.' 에러 메시지 띄워주기
    - 유효하지 않은 값일 경우 : '5~20자의 영문 소문자, 숫자와 특수기호(_), (->만 사용 가능합니다.' 에러 메시지 띄워주기

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const idEl = document.getElementById('id')
+const idMsgEl = document.getElementById('id-msg')
 const pwEl = document.getElementById('pw')
 const pwCheckEl = document.getElementById('pw-check')
 
@@ -6,4 +7,35 @@ const setFocus = (el) => {
     el.focus()
 }
 
+const isEmptyString = (value) => {
+    return value === ''
+}
+
+const isValidId = (value) => {
+    const idReg = /^[a-z0-9-_]{5,20}$/
+
+    return idReg.test(value)
+}
+
+const getErrorIdMsg = (value) => {
+    if (isEmptyString(value)) {
+        return '필수 정보입니다.'
+    }
+    if (!isValidId(value)) {
+        return '5~20자의 영문 소문자, 숫자와 특수기호(_), (-)만 사용 가능합니다.'
+    }
+
+    return null
+}
+
+const handleIdBlur = (value) => {
+    const errorMsg = getErrorIdMsg(value)
+
+    idMsgEl.innerText = errorMsg
+}
+
 setFocus(idEl)
+
+idEl.onblur = () => {
+    handleBlur(idEl.value)
+}
